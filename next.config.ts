59c5a1b46e2bddbeca787c03fe177d52000829d5
LennyKiang <kenny712126@gmail.com>
2026-01-31 18:19:57 -0500

    PLEASE WORK

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/kennyis.me",
+  basePath: "/kennyis.me", // Uncomment this line if deploying to a subdirectory
   output: "export",
   reactStrictMode: true,
 };
